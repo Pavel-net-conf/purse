98092d442a8c1fe4557ae6bdbfc26b2ab5f00e35
Pavel-net-conf <75698132+Pavel-net-conf@users.noreply.github.com>
2021-11-15 18:22:58 +0300

    V 0.3.
    Add new function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from tkinter import ttk
-
+import sqlite3
 
 class Main(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
         self.init_main()
+        self.db = db
+        self.view_records()
 
     def init_main(self):
         toolbar = tk.Frame(bg='#d7d8e0', bd=2)
@@ -16,8 +18,8 @@ class Main(tk.Frame):
                                     compound=tk.TOP, image=self.add_img)
         btn_open_dialog.pack(side=tk.LEFT)
 
-        self.tree = ttk.Treeview(self, columns=('ID', 'description', 'costs', 'total'),
-                                 height=15, show='headings')
+        self.tree = ttk.Treeview(self, columns=('ID', 'description', 'costs', 'total'), height=15, show='headings')
+
         self.tree.column('ID', width=30, anchor=tk.CENTER)
         self.tree.column('description', width=365, anchor=tk.CENTER)
         self.tree.column('costs', width=150, anchor=tk.CENTER)
@@ -30,6 +32,15 @@ class Main(tk.Frame):
 
         self.tree.pack()
 
+    def records(self, description, costs, total):
+        self.db.insert_data(description, costs, total)
+        self.view_records()
+
+    def view_records(self):
+        self.db.c.execute("""SELECT * FROM finance""")
+        [self.tree.delete(i) for i in self.tree.get_children()]
+        [self.tree.insert('', 'end', values=row) for row in self.db.c.fetchall()]
+
     def open_dialog(self):
         Child()
 
@@ -38,6 +49,7 @@ class Child(tk.Toplevel):
     def __init__(self):
         super().__init__(root)
         self.init_child()
+        self.view = app
 
     def init_child(self):
         self.title('Добавить доходы/расходы')
@@ -57,7 +69,7 @@ class Child(tk.Toplevel):
         self.entry_money = ttk.Entry(self)
         self.entry_money.place(x=200, y=110)
 
-        self.combobox = ttk.Combobox(self, volues=[u'Доходы', u'Расходы'])
+        self.combobox = ttk.Combobox(self, values=[u'Доход', u'Расход'])
         self.combobox.current(0)
         self.combobox.place(x=200, y=80)
 
@@ -66,14 +78,26 @@ class Child(tk.Toplevel):
 
         btn_ok = ttk.Button(self, text='Добавить')
         btn_ok.place(x=220, y=170)
-        btn_ok.bind('<Button-1>')
+        btn_ok.bind('<Button-1>', lambda event: self.view.records(self.entry_description.get(), self.entry_money.get(), self.combobox.get()))
 
         self.grab_set()
         self.focus_set()
 
+class DB:
+    def __init__(self):
+        self.conn = sqlite3.connect('finance.db')
+        self.c = self.conn.cursor()
+        self.c.execute("""CREATE TABLE IF NOT EXISTS finance (id integer primary key, description text, costs text, total real)""")
+        self.conn.commit()
+
+    def insert_data(self, description, costs, total):
+        self.c.execute("""INSERT INTO finance(description, costs, total) VALUES (?, ?, ?)""",
+                       (description,costs,total))
+        self.conn.commit()
 
 if __name__ == '__main__':
     root = tk.Tk()
+    db = DB()
     app = Main(root)
     app.pack()
     root.title('Финансы')
